fdb4cee20eb96e02d25026fe2f9bdfe9c93a59d8
romankalab <roan.kalab@gmail.com>
2019-10-14 08:44:52 +0200

    Initial

---
+++ Open-Lab-02.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace skuška
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] array1 = new float[] { 1.154f, 32.5f, 515.2456f, 7.1254f, 91.254f };
+            Console.WriteLine(array1[1]);
+        }
+    }
+}
